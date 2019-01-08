8aee724bb1cf7b0be25adf6040e9794fa05f9ec3
henhanwan <handiiirawan@gmail.com>
2019-01-09 00:45:41 +0700

    Change Modal Title
    change modal title to Edit Item

--- application/views/admin/dashboard/inventorylist.php
+++ application/views/admin/dashboard/inventorylist.php
@@ -113,7 +113,6 @@
   <div class="modal-dialog modal-adduser">
     <div class="modal-content">
       <div class="modal-header">
-        <h4 class="modal-title">Edit User</h4>
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
       </div>
       <div class="modal-body">
